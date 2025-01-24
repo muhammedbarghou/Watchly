155ad2165b9028df76c97412e49a42f5fa4be602
Muhammed Barghou <muhammedbarghou@gmail.com>
2025-01-24 09:34:39 +0100

    feat: add useVideoSyncRoom hook for real-time video synchronization and room management; integrate Firebase Realtime Database

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
+
+
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -21,4 +24,8 @@ const googleProvider = new GoogleAuthProvider();
 // Initialize Firestore
 const db = getFirestore(app);
 
+const rtdb = getDatabase(app);
+
+
 export { auth, googleProvider, db };
+export { rtdb };
