70fb7ca1980130e6f35e3346e0bbe33c83021d51
xcyberspy <xxdeepboyxx@gmail.com>
2025-02-06 23:13:14 +0100

    v1

--- src/components/ui/loader.tsx
+++
@@ -1,5 +0,0 @@
-export function Loader({ className }: { className?: string }) {
-    return (
-      <div className={`animate-spin rounded-full border-4 border-t-transparent ${className}`} />
-    );
-  }
